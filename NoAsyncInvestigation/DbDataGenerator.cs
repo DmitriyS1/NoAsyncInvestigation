c74716fdb60d89293f263df22f95531cc61882c6
Denis Sokolov <SokolovDenisDmitrievich@outlook.com>
2021-07-02 19:05:17 +0300

    implement data generator

---
+++ NoAsyncInvestigation/DbDataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoAsyncInvestigation
+{
+    public class DbDataGenerator
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private const int Count = 15000;
+
+        public DbDataGenerator(ApplicationDbContext dbContext)
+            => _dbContext = dbContext;
+
+        public void SetData()
+        {
+            var data = new List<Model>(Count);
+            var random = new Random();
+
+            for(var i = 0; i < Count; i++)
+            {
+                data.Add(new Model
+                {
+                    Price = random.Next()
+                });
+            }
+            //_dbContext.Database
+        }
+    }
+}
